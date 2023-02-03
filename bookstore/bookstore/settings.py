bb20fb7628ce9086382c4fe91a390b97b672bdcd
ztsv-av <ztsv.1337@gmail.com>
2023-02-02 22:59:01 -0700

    add login functionality and make 1 header file

--- bookstore/bookstore/settings.py
+++ bookstore/bookstore/settings.py
@@ -27,7 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website',
-    # 'members',
+    'members',
 ]
 
 MIDDLEWARE = [
--- bookstore/bookstore/urls.py
+++ bookstore/bookstore/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('members/', include('members.urls')),
     path('', include('website.urls')),
+    path('members/', include('django.contrib.auth.urls')),
 ]
