f492bb603acb39dc7a92c8e3fcb23da678b42c70
ztsv-av <ztsv.1337@gmail.com>
2023-02-03 23:04:02 -0700

    add logout

--- bookstore/members/urls.py
+++ bookstore/members/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('login_user', views.login_user, name="login_user"),
+    path('logout_user', views.logout_user, name="logout_user"),
 ]
--- bookstore/members/views.py
+++ bookstore/members/views.py
@@ -9,11 +9,12 @@ def login_user(request):
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
+
         if user is not None:
             login(request, user)
             return redirect('index')
         else:
-            messages.success(request, ("There Was An Error Logging In, Try Again..."))
+            messages.success(request, ("Incorrect Credentials, Try Again..."))
             return redirect('login_user')
     
     else:
@@ -22,3 +23,10 @@ def login_user(request):
             'authenticate/login_user.html',
             {}
         )
+
+
+def logout_user(request):
+
+    logout(request)
+    messages.success(request, ("You Were Logged Out"))
+    return redirect('index')
