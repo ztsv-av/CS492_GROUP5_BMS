411d0563c0d6b97ddc6643470da82737897a86d3
ctu-bms <ztsv.misc@gmail.com>
2023-02-08 21:43:57 -0800

    fix admin area

--- bookstore/bookstore/urls.py
+++ bookstore/bookstore/urls.py
@@ -3,9 +3,9 @@ from django.urls import path, include
 from cart.views import add_to_cart
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('members/', include('members.urls')),
     path('members/', include('django.contrib.auth.urls')),
     path('add_to_cart/<int:book_id>/', add_to_cart, name='add_to_cart'),
     path('', include('website.urls')),
-    path('admin/', admin.site.urls),
 ]
