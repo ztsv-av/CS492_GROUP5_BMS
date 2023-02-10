76665a1618ae70ce80632b191a11d895072b4489
ztsv-av <ztsv.1337@gmail.com>
2023-02-09 20:36:57 -0700

    fix inventory check

--- bookstore/time_jobs/jobs.py
+++ bookstore/time_jobs/jobs.py
@@ -2,7 +2,6 @@ from django.conf import settings
 import requests
 import json
 import random
-from ast import literal_eval
 from website.models import MfgInventory, Book
 
 
@@ -12,30 +11,25 @@ def check_book():
 
     for book in books:
 
-        quantity = literal_eval(book.quantity)
+        quantity = int(book.quantity)
 
         if quantity < 5:
 
-            print('Need to Order ' + book.title + ' from manufacturers!')
+            book_mfg = MfgInventory.objects.get(pk=book.id)
 
-        if quantity == 0:
-            book.available == 0
-            book.save()
-
-            print('Book ' + book.title + ' is unavailable in inventory!')
-
-
-def check_mfg():
-
-    mfg = list(MfgInventory.objects.all())
-
-    for book in mfg:
-
-        quantity = literal_eval(book.quantity)
-
-        if quantity == 0:
-            book.available = 0
-            book.save()
-
-            print('Book ' + book.title + ' is unavailable from the manufacturer!')
+            if book_mfg.quantity > 20:
+                book_mfg.quantity -= 20
+                book.quantity += 20
             
+            elif book_mfg.quantity > 0:
+                book.quantity += book_mfg.quantity
+                book_mfg.quantity = 0
+            
+            else:
+                book_mfg.available = 0
+                print('Book ' + book.title + ' is unavailable to order from the manufacturer!')
+            
+            book.save()
+            book_mfg.save()
+
+            print('Book ID:' + str(book.id) + '; Title: ' + book.title + ' was ordered from the manufacturers!')
--- bookstore/time_jobs/updater.py
+++ bookstore/time_jobs/updater.py
@@ -3,10 +3,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from .jobs import *
 
 
-scheduler = BackgroundScheduler()
-scheduler.add_job(check_book, 'interval', seconds=86400)
-scheduler.add_job(check_mfg, 'interval', seconds=86400)
-
-
 def start_scheduler():
+	scheduler = BackgroundScheduler()
+	scheduler.add_job(check_book, 'interval', seconds=10)
 	scheduler.start()
